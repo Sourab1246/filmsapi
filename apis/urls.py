d995c05211cf9f40253530cc3836c353b2b33b20
Sourab1246 <113242093+Sourab1246@users.noreply.github.com>
2023-03-30 23:17:02 +0530

    filmsapi2

--- apis/urls.py
+++ apis/urls.py
@@ -1,16 +1,18 @@
-from django.urls import path
-from .views import Actors_list,Movies_list,Movies_details
-from apis import views
+from django.urls import path, include
+from rest_framework import routers
+from .views import Movies_list, Actors_list,Movies_details,Movies
+
+
+router = routers.DefaultRouter()
+# router.register(r'Actors',Actors_list, basename='Actors_list' )
+# router.register(r'Movies', Movies_list, basename='Movies_list')
+# # router.register(r'Movies', Movies_details, basename='Movies')
+
+
+
 
 urlpatterns = [
-    path('Movies/',views.Movies_list()),
-    path('Actors/',views.Actors_list()),
-    # path('/',Actors_list.as_view()),
+    path('', include(router.urls)),
+    # path('Movies_details/', Movies),
     
-]   
-    
-    
-    
-    
-    
-
+]
